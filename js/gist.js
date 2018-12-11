fa555d5588b2c78f24e5ad99c2e59f057663e302
Lian <Lian.Duan@alumni.ubc.ca>
2018-12-10 20:25:13 -0800

    Completed Gist

--- js/gist.js
+++ js/gist.js
@@ -5,10 +5,21 @@ function gist() {
     alert("Analyzing");
     const input = document.getElementById('paragraph').value;
     var arrList = splitStringIntoArray(input);
-    var  gist = analyzeArrayforStopWords(arrList).join("");
+    analyzeArrayforStopWords(arrList);
+    var gist = returnArrayOfLowestFourScores().join(" ");
     alert(gist)
 
     document.getElementById('gist').value = gist;
+}
+function returnArrayOfLowestFourScores() {
+    var finalArrayValues = []
+
+    for (var i = 0; i < 4; i++){
+        var minkey = Math.min(... stringFreqStopStringMap.keys());
+        finalArrayValues.push(stringFreqStopStringMap.get(minkey));
+        stringFreqStopStringMap.delete(minkey);
+    }
+    return finalArrayValues;
 }
 
 //splits the string into substrings by sentence
@@ -20,11 +31,11 @@ function splitStringIntoArray(str) {
 //checks the sentences for substrings that contain stop words
 function analyzeArrayforStopWords(arr) {
     //stop words, extracted from http://snowball.tartarus.org/algorithms/english/stop.txt
-    var stopWords = ["i","me","mine","myself","we", "us", "ourselves ",
-        "ours ", "our ", "you ", "your", "yours ", "yourself", "yourselves ","he","himself",
-        "it ","its ","itself ","they","them ","their ","theirs ","themselves ","what",
-        "which","who","whom","this","that", "these","those", "am", "is", "are", "was",
-        "were","be", "been", "being", "have", "has", "had", "having", "do", "does", "did",
+    var stopWords = ["i", "me", "mine", "myself", "we", "us", "ourselves ",
+        "ours ", "our ", "you ", "your", "yours ", "yourself", "yourselves ", "he", "himself",
+        "it ", "its ", "itself ", "they", "them ", "their ", "theirs ", "themselves ", "what",
+        "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was",
+        "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did",
         "doing", "i'm", "you're", "he's", "she's", "it's", "we're", "they're", "i've", "you've",
         "we've", "they've", "i'd", "you'd", "he'd", "she'd", "we'd", "they'd", "i'll", "you'll",
         "she'll", "we'll", "they'll", "isn't", "aren't", "wasn't", "weren't", "hasn't", "haven't",
@@ -36,24 +47,32 @@ function analyzeArrayforStopWords(arr) {
         "out", "on", "off", "over", "under", "again", "then", "once", "here", "there", "when",
         "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no",
         "nor", "not", "only", "own", "same", "so", "than", "too", "very", "the"];
-    //Initialize and empty list
-    var output = [];
-    for (var i= 0; i< arr.length; i++) {
-        if (containsAny(arr[i].trim().toLowerCase(), stopWords)){
-            output.push(arr[i]);
+
+    for (var i = 0; i < arr.length; i++) {
+        var stopScore = scoreArray(arr[i], stopWords);
+        //Score is classifed as a map that contains [stopscore, <sentence>}
+            stringFreqStopStringMap.set(stopScore, arr[i]);
         }
     }
-    return output;
+function scoreArray(str, arr) {
+    var strValue = 0;
+    for (var i = 0; i < arr.length; i++) {
+        //If it does not include the "word " or the "word.", then we increase the score for the array.
+        if (!str.includes(arr[i])) {
+            strValue++
+        }
+    }
+    return strValue;    
 }
 
-function containsAny(str, arr) {
-    var contains = true
-    for (var i = 0; i < arr.length; i++){
-        if (str.includes(arr[i]+" ")){
-            contains = false;
-            return contains;
-        }
-    }
-    return contains;
-}
+// function containsAny(str, arr) {
+//     var contains = true
+//     for (var i = 0; i < arr.length; i++){
+//         if (str.includes(arr[i]+" ")){
+//             contains = false;
+//             return contains;
+//         }
+//     }
+//     return contains;
+// }
 
